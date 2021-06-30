283fe4e41182e77685af93c9fb880c11f27af17e
globe <globehead564@gmail.com>
2021-06-29 23:01:03 -0600

    added charge fire mechanic

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -12,13 +12,13 @@ var gravity = 800
 var aim = false
 var firedelay = 0
 var shaderAbberation = 0
-
 var hp = 3
 var hit = false
 var hitVector = Vector2()
-
 var camera_shake = 1
-
+var charge_time = 0
+var charge = false
+var required_charge = 0.75
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -28,6 +28,7 @@ func _ready():
 func get_input():
 	velocity.x = 0
 	aim = Input.is_action_pressed("aim")
+	charge = Input.is_action_pressed("fire")
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 	if Input.is_action_pressed("right"):
@@ -36,25 +37,33 @@ func get_input():
 	if Input.is_action_pressed("left"):
 		$AnimatedSprite.flip_h = true
 		velocity.x -= speed
-	if Input.is_action_just_pressed("fire") and aim and firedelay <= 0:
+	if Input.is_action_just_released("fire") and aim and firedelay <= 0:
 		shoot()
 
 func shoot():
-	$Gunfire.playing = true
-	shaderAbberation = 2
-	firedelay = 2
-	for i in $Smoke/Area2D.get_overlapping_bodies():
-		if "hit" in i:
-			i.hit = true
-	if get_global_mouse_position() > global_position:
-		recoil = Vector2(-800, 0)
+	if charge_time > required_charge:
+		$Gunfire.playing = true
+		shaderAbberation = 2
+		firedelay = 0.2
+		for i in $Smoke/Area2D.get_overlapping_bodies():
+			if "hit" in i:
+				i.hit = true
+		if get_global_mouse_position() > global_position:
+			recoil = Vector2(-800, 0)
+		else:
+			recoil = Vector2(800, 0)
+		$Smoke.emitting = true
+		$Camera2D/ColorRect.visible = true
+		yield(get_tree().create_timer(0.1), "timeout")
+		$Camera2D/ColorRect.visible = false
+		$Smoke.emitting = false
 	else:
-		recoil = Vector2(800, 0)
-	$Smoke.emitting = true
-	$Camera2D/ColorRect.visible = true
-	yield(get_tree().create_timer(0.1), "timeout")
-	$Camera2D/ColorRect.visible = false
-	$Smoke.emitting = false
+		for i in $Smoke/Area2D.get_overlapping_bodies():
+			if "hit" in i:
+				i.hit = true
+		$Smoke.emitting = true
+		yield(get_tree().create_timer(0.001), "timeout")
+		$Smoke.emitting = false
 
 func set_health(hp):
 	$ParallaxBackground/ParallaxLayer/Hearts.animation = "flash"
@@ -63,6 +72,12 @@ func set_health(hp):
 
 
 func _physics_process(delta):
+	if charge:
+		$Smoke2.emitting = true
+		charge_time += delta
+	else:
+		charge_time = 0
+		$Smoke2.emitting = false
 	if hit:
 		hit = false
 		hp -= 1
@@ -88,9 +103,11 @@ func _physics_process(delta):
 	if aim and get_global_mouse_position() > global_position:
 		$AnimatedSprite.flip_h = false
 		$Smoke.position.x = 529
+		$Smoke2.position.x = 16
 	elif aim:
 		$AnimatedSprite.flip_h = true
 		$Smoke.position.x = -529
+		$Smoke2.position.x = -16
 	if is_on_floor() and not velocity.x == 0:
 		if aim:
 			$AnimatedSprite.animation = "walk_aim"
